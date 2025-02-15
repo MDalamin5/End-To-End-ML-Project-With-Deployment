18273e263bc25c37475756b8e024e5b9be31be45
MDalamin5 <mdal.amin5@northsouth.edu>
2025-02-15 22:50:57 +0600

    now working on data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,28 @@
 # this file is for data transformation like encoding, scaling, missing value impute etc.
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+import pickle
+
+@dataclass  ## what is dataclass and why its required?
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+    
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+        
+    def get_data_transformer_object(self):
+        try:
+            pass
+        except:
+            pass
